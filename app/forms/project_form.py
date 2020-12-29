6b587a7b8d79f9c0e5fd393c1a838014aa808953
James Lee <jamestlee513@gmail.com>
2020-12-29 12:39:47 -0800

    Progress on project_form. Now testing

--- app/forms/project_form.py
+++ app/forms/project_form.py
@@ -3,11 +3,11 @@ from wtforms import StringField, DecimalField, DateField
 from wtforms.validators import DataRequired, Email, ValidationError
 
 class ProjectForm(FlaskForm):
-    userId = StringField('userId', validators=[DataRequired()])
-    name = StringField('name', validators=[DataRequired()])
+    userId = StringField('userId')
+    title = StringField('title', validators=[DataRequired()])
     description = StringField('description')
     fundingGoal = DecimalField('fundingGoal', validators=[DataRequired()])
-    balance = DecimalField('balance', validators=[DataRequired()])
+    balance = DecimalField('balance')
     image = StringField('image')
     dateGoal = DateField('dateGoal', validators=[DataRequired()])
     category = StringField('category', validators=[DataRequired()])
--- app/routes/api/project_routes.py
+++ app/routes/api/project_routes.py
@@ -1,24 +1,34 @@
-from flask import Blueprint, jsonify, redirect
+from flask import Blueprint, jsonify, redirect, request
 from app.models import db, Project
 from app.forms.project_form import ProjectForm
 
 project_routes = Blueprint('projects', __name__)
 
-
 @project_routes.route('/')
-def test():
-    return {"hello": "from project_routes.py!"}
+def getAllProjects():
+    return 'this works'
+    # result = Project.all()
+    # return jsonify(result)
 
 
-@project_routes.route('/new', methods=["POST"])
+@project_routes.route('/', methods=["POST"])
 def newProject():
-    form = ProjectForm()
+    form = ProjectForm() # How does this thing connect to the incoming data...?
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        data = Project()
-        form.populate_obj(data)
-        db.session.add(data)
+        project = Project(
+            user_id=form.data['userId'],
+            title=form.data['title'],
+            description=form.data['description'],
+            funding_goal=form.data['fundingGoal'],
+            balance=0.00,
+            image=form.data['image'],
+            date_goal=form.data['dateGoal'],
+            category=form.data['category']
+        )
+        db.session.add(project)
         db.session.commit()
-        return redirect("/")
+        return project.to_dict()
     print(form.errors)
-    return "Bad Data"
+    return jsonify(form.errors)
     
