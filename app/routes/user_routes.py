af07dc958ce3464394ad2ab9e4bf74d425f89fb4
Miguel Munoz <memg92@gmail.com>
2020-12-28 21:25:33 -0300

    deleted unnecessary user routes, replaced auth routes with users routes to align with our documentation

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,19 +1,85 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask import Blueprint, jsonify, session, request
+from app.models import User, db
+from app.forms import LoginForm
+from app.forms import SignUpForm
+from flask_login import current_user, login_user, logout_user, login_required
 
 user_routes = Blueprint('users', __name__)
 
 
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
+
+
 @user_routes.route('/')
-@login_required
-def users():
-    users = User.query.all()
-    return {"users": [user.to_dict() for user in users]}
+def authenticate():
+    """
+    Authenticates a user.
+    """
+    if current_user.is_authenticated:
+        return current_user.to_dict()
+    return {'errors': ['Unauthorized']}, 401
 
 
-@user_routes.route('/<int:id>')
-@login_required
-def user(id):
-    user = User.query.get(id)
-    return user.to_dict()
+@user_routes.route('/login', methods=['POST'])
+def login():
+    """
+    Logs a user in
+    """
+    form = LoginForm()
+    print(request.get_json())
+    # Get the csrf_token from the request cookie and put it into the
+    # form manually to validate_on_submit can be used
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        # Add the user to the session, we are logged in!
+        user = User.query.filter(User.email == form.data['email']).first()
+        login_user(user)
+        return user.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@user_routes.route('/logout')
+def logout():
+    """
+    Logs a user out
+    """
+    logout_user()
+    return {'message': 'User logged out'}
+
+
+@user_routes.route('/signup', methods=['POST'])
+def sign_up():
+    """
+    Creates a new user and logs them in
+    """
+    form = SignUpForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        user = User(
+            firstname=form.data['firstname'],
+            lastname=form.data['lastname'],
+            username=form.data['username'],
+            email=form.data['email'],
+            password=form.data['password']
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return user.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}
+
+
+@user_routes.route('/unauthorized')
+def unauthorized():
+    """
+    Returns unauthorized JSON when flask-login authentication fails
+    """
+    return {'errors': ['Unauthorized']}, 401
--- react-app/src/services/auth.js
+++ react-app/src/services/auth.js
@@ -1,38 +1,37 @@
-export const authenticate = async() => {
-  const response = await fetch('/api/auth/',{
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  return await response.json();
-}
-
-export const login = async (email, password) => {
-  const response = await fetch('/api/auth/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      email,
-      password
-    })
-  });
-  return await response.json();
-}
-
-export const logout = async () => {
-  const response = await fetch("/api/auth/logout", {
+export const authenticate = async () => {
+  const response = await fetch("/users/", {
     headers: {
       "Content-Type": "application/json",
-    }
+    },
   });
   return await response.json();
 };
 
+export const login = async (email, password) => {
+  const response = await fetch("/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+  return await response.json();
+};
+
+export const logout = async () => {
+  const response = await fetch("/users/logout", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return await response.json();
+};
 
 export const signUp = async (username, email, password) => {
-  const response = await fetch("/api/auth/signup", {
+  const response = await fetch("/users/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -44,4 +43,4 @@ export const signUp = async (username, email, password) => {
     }),
   });
   return await response.json();
-}
+};
