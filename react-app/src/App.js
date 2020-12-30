759d52b75d2465fa85030d52680294c865b23de3
Miguel Munoz <memg92@gmail.com>
2020-12-30 14:15:01 -0300

    fixed ProtectedRoute bug that tried to render all Protected components, added a switch to browser router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
-import NewProject from "./components/NewProject"
-import ProjectProfile from './components/ProjectProfile'
+import NewProject from "./components/NewProject";
+import ProjectProfile from "./components/ProjectProfile";
 import { authenticate } from "./services/auth";
 
 function App() {
@@ -32,38 +32,49 @@ function App() {
     <BrowserRouter>
       <NavBar
         authenticated={authenticated}
-        setAuthenticated={setAuthenticated} />
-      <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-        <h1>SeedFund</h1>
-      </ProtectedRoute>
-      <Route path="/login" exact={true}>
-        <LoginForm
+        setAuthenticated={setAuthenticated}
+      />
+      <Switch>
+        <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
+          <h1>SeedFund</h1>
+        </ProtectedRoute>
+        <Route path="/login" exact={true}>
+          <LoginForm
+            authenticated={authenticated}
+            setAuthenticated={setAuthenticated}
+          />
+        </Route>
+        <Route path="/signup" exact={true}>
+          <SignUpForm
+            authenticated={authenticated}
+            setAuthenticated={setAuthenticated}
+          />
+        </Route>
+        <ProtectedRoute
+          path="/start"
+          exact={true}
           authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-      </Route>
-      <Route path="/signup" exact={true}>
-        <SignUpForm
+        ></ProtectedRoute>
+        <ProtectedRoute
+          path="/profile"
+          exact={true}
           authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-      </Route>
-      <ProtectedRoute
-        path="/start"
-        exact={true}
-        authenticated={authenticated}
-      ></ProtectedRoute>
-      <ProtectedRoute
-        path="/profile"
-        exact={true}
-        authenticated={authenticated}
-      ></ProtectedRoute>
-      <ProtectedRoute path="/start" exact={true} authenticated={authenticated}>
-        <NewProject />
-      </ProtectedRoute>
-      <ProtectedRoute path='/project/:id' exact={true} authenticated={authenticated}>
-        <ProjectProfile />
-      </ProtectedRoute>
+        ></ProtectedRoute>
+        <ProtectedRoute
+          path="/start"
+          exact={true}
+          authenticated={authenticated}
+        >
+          <NewProject />
+        </ProtectedRoute>
+        <ProtectedRoute
+          path="/project/:id"
+          exact={true}
+          authenticated={authenticated}
+        >
+          <ProjectProfile />
+        </ProtectedRoute>
+      </Switch>
     </BrowserRouter>
   );
 }
--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Route, Redirect, useLocation } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
-  const { pathname } = useLocation();
-  if (!props.authenticated && pathname !== "/signup") {
-    return <Redirect to="/login" />;
-  }
-
-  return <Route {...props} />;
+  return (
+    <Route {...props}>
+      {props.authenticated ? props.children : <Redirect to="/login" />}
+    </Route>
+  );
 };
 
 export default ProtectedRoute;
