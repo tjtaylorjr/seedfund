d7a9f1622f8db48fd6d2d2df511787a139d992ee
TJ Taylor <tjtaylorjr@gmail.com>
2020-12-30 11:36:25 -0500

    WIP

---
+++ react-app/src/components/CategoryQSelect.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const CategoryQSelect = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <div></div>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink></NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryQSelect;
