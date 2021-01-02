ad49e02199f4148d5748c7a96b3c1a0f158db672
TJ Taylor <tjtaylorjr@gmail.com>
2021-01-02 03:25:28 -0500

    working on improving search functionality and adding seed examples

--- app/routes/api/project_routes.py
+++ app/routes/api/project_routes.py
@@ -4,7 +4,7 @@ from app.models import db, Project, User
 from app.forms.project_form import ProjectForm
 from flask_login import current_user
 from sqlalchemy.orm import joinedload
-from sqlalchemy import func
+from sqlalchemy import func, or_
 
 project_routes = Blueprint('projects', __name__)
 
@@ -92,9 +92,9 @@ def deleteProject(id):
     else:
         return {"error": f'id {id} not found'}
 
-
+# a little more robust but still not perfect.  Will search in multiple places on table but can't handle multiple search keywords well yet
 @project_routes.route('/search/<query>')
 def searchForProjects(query):
-    result = Project.query.filter(Project.title.ilike(f"%{query}%")).options(joinedload(Project.user)).all()
+    result = Project.query.filter(or_(Project.title.ilike(f"%{query}%"),Project.description.ilike(f"%{query}%"),Project.category.ilike(f"%{query}%"))).options(joinedload(Project.user)).all()
     data = [ project.to_dict() for project in result ]
     return {"projects": data}
