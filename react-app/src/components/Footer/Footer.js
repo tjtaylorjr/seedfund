2621a828822fc13813e592af56883f790ff0a42a
TJ Taylor <tjtaylorjr@gmail.com>
2021-09-27 10:41:23 -0400

    update copyright

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
         <Community />
       </div>
       <div className="footer__bottom-container">
-        <div className="footer-copyright">SeedFund, © 2020</div>
+        <div className="footer-copyright">SeedFund, © 2021</div>
         <a
           className="footer-github"
           href="https://github.com/tjtaylorjr/seedfund"
